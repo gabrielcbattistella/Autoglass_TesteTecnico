253e6bdc8f4102f30eeeefaed292a710b02f07be
Gabriel Chüler Battistella <gabriel.chulerbattistella@gmail.com>
2024-03-08 21:09:35 -0300

    Teste unitário do Filtro paginado

--- Tests/Services/ServiceProductTest.cs
+++ Tests/Services/ServiceProductTest.cs
@@ -4,6 +4,8 @@ using Moq;
 using Domain.Entities;
 using Domain.Interfaces.Repositories;
 using Domain.Services.Services;
+using Domain.BaseResponse;
+using System;
 
 namespace Tests.Services
 {
@@ -89,6 +91,43 @@ namespace Tests.Services
 
             // Assert
             repositoryMock.Verify(repo => repo.Update(product), Times.Once);
+        }
+
+        [Fact]
+        public void Filter_ReturnsPagedResultFromRepository()
+        {
+            // Arrange
+            var expectedProducts = new List<Product>
+            {
+                new Product { CodigoProduto = 1 },
+                new Product { CodigoProduto = 2 },
+                new Product { CodigoProduto = 3 }
+            };
+            var expectedTotalCount = 10;
+            var entity = new Product();
+            var page = 1;
+            var pageSize = 5;
+            var repositoryMock = new Mock<IRepositoryProduct>();
+            repositoryMock.Setup(repo => repo.FilterProducts(entity, page, pageSize))
+                          .Returns(new PagedResult<Product>
+                          {
+                              Results = expectedProducts,
+                              CurrentPage = page,
+                              PageCount = (int)Math.Ceiling((double)expectedTotalCount / pageSize),
+                              PageSize = pageSize,
+                              RowCount = expectedTotalCount
+                          });
+            var service = new ServiceProduct(repositoryMock.Object);
+
+            // Act
+            var result = service.Filter(entity, page, pageSize);
+
+            // Assert
+            Assert.Equal(expectedProducts, result.Results);
+            Assert.Equal(page, result.CurrentPage);
+            Assert.Equal((int)Math.Ceiling((double)expectedTotalCount / pageSize), result.PageCount);
+            Assert.Equal(pageSize, result.PageSize);
+            Assert.Equal(expectedTotalCount, result.RowCount);
         }
     }
 }
